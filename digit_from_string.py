5f3659724133995d9a49e3285d83bdac8e42afd6
Armen Mkhoyan <74472105+Armenmkhoyan@users.noreply.github.com>
2023-01-09 15:15:24 +0400

    task_1

--- digit_from_string.py
+++ digit_from_string.py
@@ -1,16 +1,21 @@
 import re
 
 
-def find_digit_from_string(text: str) -> list[int]:
+def sum_from_string(texts: str) -> int:
+    digits = find_digits_from_text(texts)
+    return sum(digits)
+
+
+def find_digits_from_text(texts: str) -> list[int]:
     temp = []
-    for digit in re.findall(r'-?\d+', text):
+    for digit in re.findall(r'-?\d+', texts):
         try:
             temp.append(int(digit))
-        except:
-            pass
+        except ValueError as e:
+            print(e)
     return temp
 
 
 if __name__ == "__main__":
     text = '-100#^sdfkj8902w3ir021@swf-20'
-    print(sum(find_digit_from_string(text)))
+    print(sum_from_string(text))
